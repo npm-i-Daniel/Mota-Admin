0c9a006920f542d855482fd36496eff1e35fe93b
npm-i-Daniel <sdaniel28910@gmail.com>
2025-08-14 23:34:11 +0530

    first change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let nav = document.querySelector("nav")
+let nav = document.querySelector(".header nav")
 
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 0) {
         nav.classList.add("sticky")
     } else {
         nav.classList.remove("sticky")
